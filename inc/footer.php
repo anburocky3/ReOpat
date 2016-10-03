55bc46d1d55c61edf8caa98e6555acc373b3823d
Anbuselvan Rocky <anbustudios3@gmail.com>
2016-10-03 17:06:15 +0530

    Important configuration files

---
+++ inc/footer.php
@@ -0,0 +1,97 @@
+<style>
+html {
+  position: relative;
+  min-height: 100%;
+}
+body {
+  /* Margin bottom by footer height */
+  margin-bottom: 60px;
+}
+.footer {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  /* Set the fixed height of the footer here */
+  height: 60px;
+  color:white;
+  background-color: white;
+  border-top: 3px solid #E8E8E8;
+}
+a
+{
+	text-decoration: none;
+	color: #A1A1A1;
+}
+</style>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+<footer class="footer">
+<div style="width:100%; bottom:0px;" class="container-fluid">
+<div class="row">
+<div class="col-sm-4">
+<div align="left">
+<br />
+
+<ul>
+<li style="display:inline-block;"><a href="about" style="text-decoration:none;">About Us |</a></li><li style="display:inline-block;">
+<a href="contact" style="text-decoration:none;">&nbsp;Contact Us |</a></li><li style="display:inline-block;">
+<a style="text-decoration:none;" href="#team" data-toggle="modal">&nbsp;Developed by ReOpat Team</a></li>
+</ul>
+
+</div>
+</div>
+<div style="color:#828282;" class="col-sm-4"><br /><p align="center">ReOpat - All Rights Reserved.</p></div>
+<div class="col-sm-4">
+<div align="left">
+<br />
+
+<ul>
+<li style="display:inline-block;"><a style="text-decoration:none;">RTI |</a></li><li style="display:inline-block;">
+<a style="text-decoration:none;" href="#terms" data-toggle="modal">&nbsp;Terms and Condition |</a></li><li style="display:inline-block;"><a style="text-decoration:none;" href="#" onclick="alert('Contact us @ reopat@gmail.com');">&nbsp;Feedback and Queries</a></li>
+</ul>
+
+<!-- modal -->
+ <div id="team" class="modal fade">
+        <div class="modal-dialog" style="color:black;">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                    <h4 class="modal-title">ReOpat Team</h4>
+                </div>
+                <div class="modal-body">
+                    <p>Crafted with care by DMI College of Engineering, CSE Students</p>
+                    <p class="text-warning"><small>Contact us for more info</small></p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- end modal -->
+    
+    <!-- modal -->
+ <div id="terms" class="modal fade">
+        <div class="modal-dialog" style="color:black;">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                    <h4 class="modal-title">Terms and Conditions</h4>
+                </div>
+                <div class="modal-body">
+                    <p>This website is designed, developed and maintained by The ReOpat Team and content provided by the public for the information to the State Government of Tamil Nadu. The documents and information displayed in this website are for reference purposes only and do not purport to a legal document. Though all efforts have been made to ensure the accuracy and currency of the content on this website, the same should not be construed as a statement of law or used for any legal purposes. In case of any ambiguity or doubts, users are advised to verify / check with the source(s), and to obtain appropriate professional advice before use of information. In case of any variance between what is stated and that contained in the relevant Acts, Rules, Regulations, Policy, Statements, etc, the latter shall prevail.Under no circumstances will this team be liable for any expense, loss or damage including, without limitation, indirect or consequential loss or damage, or any expense, loss or damage whatsoever arising from use, or loss of use, of data, arising out of or in connection with the use of this website. These terms and conditions shall be governed by and construed in accordance with the Indian Laws. Any dispute arising under these terms and conditions shall be subject to the jurisdiction of the courts of India. The information posted on this website could include hypertext links or pointers to information created and maintained by non-Government / private organisations. ReOpat is providing these links and pointers solely for your information and convenience. When you select a link to an outside website, you are leaving the ReOpat website and are subject to the privacy and security policies of the owners / sponsors of the outside website. ReOpat does not guarantee the availability of such linked pages at all times. ReOpat cannot authorize the use of copyrighted materials contained in linked websites. Users are advised to request such authorization from the owner of the linked website. ReOpat does not guarantee that linked websites comply with Indian Government Web Guidelines.ReOpat neither endorses in any way nor offers any judgement or warranty and accepts no responsibility or liability for the authenticity, availability of any of the goods or services or for any damage, loss or harm, directly or consequential or any violation of international or local laws that may be incurred by your visiting and transacting on these websites.</p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- end modal -->
+
+
+</div>
+</div>
+</div>
+</div>
+</footer>
---
+++ inc/header.php
@@ -0,0 +1,10 @@
+<nav class="navbar" style="border-bottom:2px solid blue;">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="http://localhost/reopat/index.php">ReOpat</a>
+  </div>
+</nav>
+<?php
+
+session_start();
+
+?>
